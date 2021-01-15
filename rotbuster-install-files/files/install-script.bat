741d4ee9d9b0f7d3cba92c1fee1d1308e032e862
ruben <rubencarlsen@gmail.com>
2021-01-15 16:21:55 +0100

    ikke installer

--- rotbuster-install-files/files/install-script.bat
+++
@@ -1,7 +0,0 @@
-IF EXIST %USERPROFILE%\rotbuster @RD /S /Q %USERPROFILE%\rotbuster
-mkdir %USERPROFILE%\rotbuster
-copy rotbuster.bat %USERPROFILE%\rotbuster
-copy rotbuster.jar %USERPROFILE%\rotbuster
-copy README.txt %USERPROFILE%\rotbuster
-
-setx PATH "%PATH%;%USERPROFILE%\rotbuster\"
--- rotbuster-install-files/files/rotbuster.bat
+++
@@ -1,1 +0,0 @@
-java -jar %~dp0rotbuster.jar %1 %2 %3 %4
