3eafb74447c964755ed06390dd5e202d3adc2794
ruben <rubencarlsen@gmail.com>
2021-01-15 10:49:12 +0100

    Update Help.java

--- src/Help.java
+++ src/Help.java
@@ -8,11 +8,11 @@ public class Help {
         System.out.println("\t");
         System.out.println("FUNCTIONS:");
         System.out.println("\t-h, -help \t\t\tHelp screen");
-        System.out.println("\t[EN-/DECODE]: \t\t\t-e (encode) or -d (decode)");
-        System.out.println("\t[ROT FORMAT]: \t\t\tany number or -all (Only for decode)");
-        System.out.println("\t[LANGUAGE]: \t\t\tnor or eng");
+        System.out.println("\t[EN-/DECODE]: \t\t\t-enc/enc (encode) or -dec/dec (decode)");
+        System.out.println("\t[ROT FORMAT]: \t\t\tany number or -all/all (Only for decode)");
+        System.out.println("\t[LANGUAGE]: \t\t\t-nor/nor or -eng/eng");
         System.out.println("\t[PLAINTEXT]/[CIPHERTEXT]: \t\"Text\"");
         System.out.println("\n");
-        System.out.println("Example: rotbuster -e 13 eng \"helloworld\"");
+        System.out.println("Example: rotbuster -enc 13 -eng \"helloworld\"");
     }
 }
