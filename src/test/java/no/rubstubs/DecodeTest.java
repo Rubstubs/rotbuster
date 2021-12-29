95f13c8ff276768ec2c7b08ed404d105ca46b167
ruben <rubencarlsen@gmail.com>
2021-12-29 17:21:15 +0100

    Decode.shouldDecodeRot13WithWrapEnglish 🟢

--- src/test/java/no/rubstubs/DecodeTest.java
+++ src/test/java/no/rubstubs/DecodeTest.java
@@ -6,10 +6,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DecodeTest {
     @Test
-    void shouldDecodeRot2WithoutWrap() {
+    void shouldDecodeRot2WithoutWrapEnglish() {
         String cipher = "CDE";
         String plain = Decode.decodeSpecific(2, "eng", cipher);
         assertEquals(plain, "abc");
+    }
+
+    @Test
+    void shouldDecodeRot13WithWrapEnglish() {
+        String cipher = "CDE";
+        String plain = Decode.decodeSpecific(13, "eng", cipher);
+        assertEquals(plain, "pqr");
     }
 
 }
