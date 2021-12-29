92c027e5ca13d40ad5343fdef57e2b03ca965ad3
ruben <rubencarlsen@gmail.com>
2021-12-29 15:58:21 +0100

    how about this?

--- src/test/java/no/rubstubs/TestEncode.java
+++ src/test/java/no/rubstubs/TestEncode.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class testEncode {
+public class TestEncode {
     @Test
     void shouldEncodeEnglishNoWrap() {
         String plain = "abc";
